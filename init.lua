96320789a00231f5e3e05dabfde2b7b56cdf0a10
Tzero Ocne <tzeroocne.interface@gmail.com>
2023-12-09 13:01:03 +0700

    feat(wsl): adjust config for wsl

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd([[
+augroup RestoreCursorShapeOnExit
+    autocmd!
+    autocmd VimLeave * set guicursor=a:ver30-iCursor-blinkwait300-blinkon200-blinkoff150
+augroup END
+]])
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+local ls = require("luasnip")
+
+local function expand_snippet()
+  return ls.expand()
+end
+
+map("n", "<C-\\>", "<cmd>Neotree<cr>", { desc = "Jump To Neotree" })
+map("n", "<C-w>Q", "<cmd>bd<cr>", { desc = "Close Buffer" })
+map("i", "<C-Space>", expand_snippet, { silent = true })
