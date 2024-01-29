f7692a0880a78ed36cb4d7feb15d455e0f11e42f
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-01-29 21:15:42 +0700

    feat(keymaps): add basic keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+local unmap = vim.keymap.del
+
+map("n", "<C-\\>", "<cmd>Neotree<cr>")
+map("n", "<C-w>e", "<cmd>bd<cr>", { desc = "Close Buffer" })
+map("n", "<Bslash>O", "O<Esc><Down>", { desc = "New Line Above" })
+
+unmap({ "n", "v" }, "s")
