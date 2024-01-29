0285dc83e6d22223232ffbf2352f00177c211dee
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-01-29 21:14:07 +0700

    feat(vscode): add vscode config

---
+++ init.vscode.lua
@@ -0,0 +1,7 @@
+-- Get the current working directory
+local current_dir = vim.fn.getcwd()
+
+-- Print the current directory
+print("Current Directory:", current_dir)
+
+require("config.vscode")
---
+++ lua/config/vscode.lua
@@ -0,0 +1,33 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+local default_plugins = {
+  {
+    "kylechui/nvim-surround",
+    lazy = false,
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+}
+
+require("lazy").setup(default_plugins)
