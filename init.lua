9d69cac40ef983fe5ad3c615b974ec7af5618d07
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-01-29 21:26:21 +0700

    feat(cmd): move startup script to init file

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Restore cursor to bar blink when exit
+vim.cmd([[
+augroup RestoreCursorShapeOnExit
+    autocmd!
+    autocmd VimLeave * set guicursor=a:ver30-iCursor-blinkwait300-blinkon200-blinkoff150
+augroup END
+]])
+
+-- Make some background transparent
+vim.cmd([[
+  hi CursorLine guibg=NONE
+  hi NeoTreeNormal guibg=NONE
+  hi NeoTreeNormalNC guibg=NONE
+  hi TroubleNormal guibg=NONE
+]])
