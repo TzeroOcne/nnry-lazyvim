33ba717fa56d92b2305e6e5bba5322ecf00539c7
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-05-14 15:28:23 +0700

    progress

--- init.vscode.lua
+++ init.vscode.lua
@@ -1,7 +1,12 @@
--- Get the current working directory
 local current_dir = vim.fn.getcwd()
+print("Current Directory:", current_dir)
+
+local localAppData = os.getenv('LOCALAPPDATA')
+vim.cmd('cd ' .. localAppData .. '/nvim')
+-- Get the current working directory
+current_dir = vim.fn.getcwd()
 
 -- Print the current directory
 print("Current Directory:", current_dir)
 
-require("config.vscode")
+require("lua.config.vscode")
