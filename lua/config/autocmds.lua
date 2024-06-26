11072dadf3456eb2fedf85de3ad581ffcdd83d26
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-06-27 00:13:54 +0700

    feat(nvim): bump to user nvim version 0.10.0
    user native snippet instead of luasnip

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,20 +6,20 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
-function leave_snippet()
-  if
-    ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
-    and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
-    and not require('luasnip').session.jump_active
-  then
-    require('luasnip').unlink_current()
-  end
-end
-
--- stop snippets when you leave to normal mode
-vim.api.nvim_command([[
-  autocmd ModeChanged * lua leave_snippet()
-]])
+-- function leave_snippet()
+--   if
+--     ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+--     and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+--     and not require('luasnip').session.jump_active
+--   then
+--     require('luasnip').unlink_current()
+--   end
+-- end
+--
+-- -- stop snippets when you leave to normal mode
+-- vim.api.nvim_command([[
+--   autocmd ModeChanged * lua leave_snippet()
+-- ]])
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   group = augroup("AutoFileType"),
