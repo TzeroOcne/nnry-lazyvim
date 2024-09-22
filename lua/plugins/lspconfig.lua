234fc17a96d380bceeaeb23812dda110b5273094
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-09-22 12:47:26 +0700

    feat(lspconfig): add default tailwind class

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -12,15 +12,13 @@ local lsp_options = {
       },
     },
     tailwindcss = {
-      -- exclude a filetype from the default_config
-      filetypes_exclude = { "markdown" },
-      -- add additional filetypes to the default_config
-      filetypes_include = {},
-      -- to fully override the default_config, change the below
-      -- filetypes = {}
       settings = {
         tailwindCSS = {
           classAttributes = {
+            "class",
+            "className",
+            "ngClass",
+            "class:list",
             "contentClass",
           },
         },
