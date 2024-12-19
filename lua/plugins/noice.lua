778edabcd45f40ebc8ccc5b1cda132a47757ef64
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-12-19 22:49:25 +0700

    feat(noice): customize noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,5 +10,16 @@ return {
         },
       },
     },
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = {
+          skip = true,
+        },
+      },
+    },
   },
 }
